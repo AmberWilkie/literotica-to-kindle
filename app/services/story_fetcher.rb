2b27038c87c92fab53bb5d1f9f63619a8850f1ca
amberwilkie <amber@amberwilkie.com>
2019-03-10 16:02:18 +0100

    Loop through genre links, write a bunch of relevant stories to files

--- app/services/story_fetcher.rb
+++ app/services/story_fetcher.rb
@@ -2,13 +2,16 @@ class StoryFetcher
   require 'open-uri'
 
   GENRE_LINKS = %w[
+    https://www.literotica.com/c/erotic-couplings
+    https://www.literotica.com/c/gay-sex-stories
+    https://www.literotica.com/c/mind-control
+    https://www.literotica.com/c/masturbation-stories
     https://www.literotica.com/c/non-consent-stories
   ].freeze
 
   def initialize; end
 
   def call
-    puts valid_links
     valid_links.each do |link|
       story_text = ''
       (1..100).each do |num|
@@ -25,24 +28,53 @@ class StoryFetcher
   private
 
   def titles
-    @titles ||= HTTParty.get("#{GENRE_LINKS.first}/rss")['rss']['channel']['item']
-                        .select { |item| Date.parse(item['pubDate']) == Date.today } # exclude old stories
+    @titles ||= parsed_titles
+  end
+
+  def parsed_titles
+    titles = []
+    GENRE_LINKS.each do |link|
+      titles << HTTParty.get("#{link}/rss")['rss']['channel']['item']
+                        .select { |item| published_today?(item) }
                         .map { |item| item['title'] }
+    end
+    titles.flatten.compact
   end
 
   def links
-    @links ||= Nokogiri::HTML(open(GENRE_LINKS.first)).css('h3').xpath('a')
+    @links ||= parsed_links
+  end
+
+  def parsed_links
+    links = []
+    GENRE_LINKS.each do |link|
+      url = Nokogiri::HTML(open(link)).css('h3').xpath('a')
+      links << url
+    end
+    links.flatten.compact
   end
 
   def valid_links
-    @valid_links ||= links.select { |link| titles.include?(link.children.first.text) }
+    @valid_links ||= links.select { |link| titles.include?(link_title(link)) }
                           .map { |link| link.attributes['href'].value }
-                          .reject { |link| link.last.match(/[2-9]/) } # exclude continuing chapters
+                          .reject { |link| continuing_chapter?(link) }
   end
 
   def write_to_file(title, text)
     file = File.open("#{Dir.pwd}/tmp/#{title}.txt", 'wb')
     file.puts(text)
     file.close
+  end
+
+  def link_title(link)
+    link.children.first.text
+  end
+
+  def published_today?(item)
+    Date.parse(item['pubDate']) == Date.today
+  end
+
+  def continuing_chapter?(link)
+    link.last.match(/[2-9]/)
   end
 end

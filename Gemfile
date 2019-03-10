866ef34b5e95733259b61470543bbf9ef18b2037
amberwilkie <amber@amberwilkie.com>
2019-03-10 17:49:45 +0100

    Add README, improve link parsing

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'dotenv-rails'
 gem 'httparty'
 
 group :development, :test do
--- app/services/story_fetcher.rb
+++ app/services/story_fetcher.rb
@@ -1,44 +1,34 @@
 class StoryFetcher
   require 'open-uri'
 
-  GENRE_LINKS = %w[
-    https://www.literotica.com/c/erotic-couplings
-    https://www.literotica.com/c/gay-sex-stories
-    https://www.literotica.com/c/mind-control
-    https://www.literotica.com/c/masturbation-stories
-    https://www.literotica.com/c/non-consent-stories
-  ].freeze
-
   def initialize; end
 
   def call
+    puts valid_links
     valid_links.each do |link|
       story_text = ''
+      meta = ''
       (1..100).each do |num|
         page = Nokogiri::HTML(open("#{link}?page=#{num}"))
-        break unless page.css('a.b-pager-next').any?
+        break unless num == 1 || page.css('a.b-pager-next').any?
 
+        keywords = page.at("meta[name='keywords']")['content']
+        description = page.at("meta[name='description']")['content']
+        author = page.css('span.b-story-user-y').children.last.text
+        meta = "#{keywords}\n#{description}\n#{author}\n\n" if meta.empty?
         story_text += page.css('div.b-story-body-x').text
       end
-      write_to_file(link.split('/').last, story_text)
+
+      write_to_file(link.split('/').last, story_text, meta)
       # send story text to Kindle, bundled nice so it will be readable and whatnot.
     end
   end
 
   private
 
-  def titles
-    @titles ||= parsed_titles
-  end
-
-  def parsed_titles
-    titles = []
-    GENRE_LINKS.each do |link|
-      titles << HTTParty.get("#{link}/rss")['rss']['channel']['item']
-                        .select { |item| published_today?(item) }
-                        .map { |item| item['title'] }
-    end
-    titles.flatten.compact
+  def genre_links
+    ENV['GENRE_LINKS'].split('|')
+                      .map { |link| "https://www.literotica.com/top/#{link}/last-30-days/?mode=publishes" }
   end
 
   def links
@@ -47,34 +37,33 @@ class StoryFetcher
 
   def parsed_links
     links = []
-    GENRE_LINKS.each do |link|
-      url = Nokogiri::HTML(open(link)).css('h3').xpath('a')
+    genre_links.each do |link|
+      url = Nokogiri::HTML(open(link)).css('a.title')
       links << url
     end
     links.flatten.compact
   end
 
   def valid_links
-    @valid_links ||= links.select { |link| titles.include?(link_title(link)) }
+    @valid_links ||= links.select { |link| published_twenty_days_ago?(link) }
                           .map { |link| link.attributes['href'].value }
                           .reject { |link| continuing_chapter?(link) }
   end
 
-  def write_to_file(title, text)
+  def write_to_file(title, text, meta)
     file = File.open("#{Dir.pwd}/tmp/#{title}.txt", 'wb')
+    file.puts(meta)
     file.puts(text)
     file.close
   end
 
-  def link_title(link)
-    link.children.first.text
-  end
-
-  def published_today?(item)
-    Date.parse(item['pubDate']) == Date.today
+  def published_twenty_days_ago?(item)
+    Date.strptime(item.parent.text[/\(.*?\)/].delete('(').delete(')').to_s, '%m/%d/%y') == Date.today - 20.days
+  rescue StandardError
+    false
   end
 
   def continuing_chapter?(link)
-    link.last.match(/[2-9]/)
+    link.match(/[2-9]/) && (link.match(/-ch-/i) || link.match(/-pt-/i))
   end
 end

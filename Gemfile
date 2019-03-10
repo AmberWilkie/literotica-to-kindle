47b9e2294f75bcc49edd93a5b35a88017c70419b
amberwilkie <amber@amberwilkie.com>
2019-03-10 15:35:43 +0100

    Fetch stories, scrape, write to file

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.3.1'
+ruby '>= 2.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
@@ -31,9 +31,13 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'httparty'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry'
+  gem 'pry-byebug'
 end
 
 group :development do
---
+++ app/services/story_fetcher.rb
@@ -0,0 +1,48 @@
+class StoryFetcher
+  require 'open-uri'
+
+  GENRE_LINKS = %w[
+    https://www.literotica.com/c/non-consent-stories
+  ]
+
+  def initialize;
+  end
+
+  def call
+    puts valid_links
+    valid_links.each do |link|
+      story_text = ''
+      (1..100).each do |num|
+        page = Nokogiri::HTML(open("#{link}?page=#{num}"))
+        break unless page.css('a.b-pager-next').any?
+        story_text += page.css('div.b-story-body-x').text
+      end
+      write_to_file(link.split('/').last, story_text)
+      # send story text to Kindle, bundled nice so it will be readable and whatnot.
+    end
+  end
+
+  private
+
+  def titles
+    @titles ||= HTTParty.get("#{GENRE_LINKS.first}/rss")['rss']['channel']['item']
+                  .select { |item| Date.parse(item['pubDate']) == Date.today } # exclude old stories
+                  .map { |item| item['title'] }
+  end
+
+  def links
+    @links ||= Nokogiri::HTML(open(GENRE_LINKS.first)).css('h3').xpath('a')
+  end
+
+  def valid_links
+    @valid_links ||= links.select { |link| titles.include?(link.children.first.text) }
+                       .map { |link| link.attributes['href'].value }
+                       .reject { |link| link.last.match(/[2-9]/) } # exclude continuing chapters
+  end
+
+  def write_to_file(title, text)
+    file = File.open("#{Dir.pwd}/tmp/#{title}.txt", 'wb')
+    file.puts(text)
+    file.close
+  end
+end

2cbb4964bf1d1d8394a97046114b51419a7907fc
amberwilkie <amber@amberwilkie.com>
2019-03-10 19:47:53 +0100

    Set up sending to Kindle

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV['EMAIL_FROM']
+  default to: ENV['EMAIL_TO']
   layout 'mailer'
+
+  def send_document(subject, filename)
+    attachments["#{subject}.txt"] = File.read(filename)
+    mail subject: subject
+  end
 end
